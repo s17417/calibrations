92096cea150382e02842ae80e97850565e0b1e81
tomasz poławski <tomasz.polawski@gmail.com>
2023-03-12 19:41:29 +0100

    corrected Validation annotation in CalibrationSetDTO class and added getEntityClass for retriving class type

--- src/main/java/com/krzywe/DTO/CalibrationSetDTO.java
+++ src/main/java/com/krzywe/DTO/CalibrationSetDTO.java
@@ -4,9 +4,6 @@ import java.time.LocalDate;
 
 import com.krzywe.Model.MaterialType;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,7 +14,6 @@ public class CalibrationSetDTO {
 	
 	@NotBlank
 	@Size(min=3, max=50)
-	@Column(nullable=false, length = 50)
 	private String name;
 	
 	@PastOrPresent
@@ -27,8 +23,6 @@ public class CalibrationSetDTO {
 	private LocalDate expirationDate;
 	
 	@NotNull
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
 	private MaterialType materialType;
 
 	public String getName() {
--- src/main/java/com/krzywe/Utils/Exceptions/EntityNotFoundException.java
+++ src/main/java/com/krzywe/Utils/Exceptions/EntityNotFoundException.java
@@ -6,16 +6,20 @@ public class EntityNotFoundException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private Class<?> entityClass;
 
 	public EntityNotFoundException(Class<?> entityClass) {
 		super("Entity of type: "+entityClass.getSimpleName()+" not found.");
+		this.entityClass = entityClass;
 	}
 
 	public EntityNotFoundException(Class<?> entityClass, String message) {
 		super("Entity of type:"+entityClass.getSimpleName()+"not found:"+message);
-		// TODO Auto-generated constructor stub
+		this.entityClass = entityClass;
 	}
-	
-	 
 
+	public Class<?> getEntityClass() {
+		return entityClass;
+	}
 }
