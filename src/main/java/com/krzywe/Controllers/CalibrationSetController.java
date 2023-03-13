d6b21e43fb9f1ad64f89493bb3ffa4105c84decd
tomasz poławski <tomasz.polawski@gmail.com>
2023-03-13 20:16:13 +0100

    added put and get methods to CalibrationSetController

--- src/main/java/com/krzywe/Controllers/CalibrationSetController.java
+++ src/main/java/com/krzywe/Controllers/CalibrationSetController.java
@@ -1,23 +1,39 @@
 package com.krzywe.Controllers;
 
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.krzywe.DTO.CalibrationSetDTO;
 import com.krzywe.DTO.CalibrationSetView;
+import com.krzywe.DTO.SimpleCalibrationSetView;
+import com.krzywe.Model.MaterialType;
 import com.krzywe.Service.ICalibrationSetService;
+import com.krzywe.Utils.Exceptions.EntityNotFoundException;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/v1/calibration_set")
+@Validated
 public class CalibrationSetController {
 	
 	private ICalibrationSetService service;
@@ -39,6 +55,61 @@ public class CalibrationSetController {
 						.build(result.getId())
 						)
 				.body(result);
+	}
+	
+	@PutMapping(
+			path ="/{id}",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE
+			)
+	public ResponseEntity<CalibrationSetView> update(@Valid @RequestBody @NotNull CalibrationSetDTO calibrationSetDTO, @PathVariable String id) throws EntityNotFoundException{
+		var result = service.update(calibrationSetDTO, id);
+		return ResponseEntity
+				.ok()
+				.body(result);
+	}
+	
+	@GetMapping(
+			path ="/{id}",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE
+			)
+	public ResponseEntity<CalibrationSetView> findById(@PathVariable String id) throws EntityNotFoundException{
+		return ResponseEntity
+				.ok(service.findById(id));
+	}
+	
+	@GetMapping(
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE
+			)
+	public ResponseEntity<Page<SimpleCalibrationSetView>> findAll(
+			@RequestParam(required = false) String name,
+			@RequestParam(required = false) MaterialType materialType,
+			@RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE) LocalDate preparationDateFrom,
+			@RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE) LocalDate preparationDateTo,
+			@RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime createdDateFrom,
+			@RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime createdDateTo,
+			@RequestParam(defaultValue = "0") int pageNumber,
+			@RequestParam(defaultValue = "25") int pageSize,
+			@RequestParam(defaultValue = "DESC") Direction sortDirection,
+			@RequestParam(defaultValue = "createdDate") String sortAttribute
+			){
+		var result = service.findAll(
+				name,
+				materialType,
+				preparationDateFrom,
+				preparationDateTo,
+				createdDateFrom,
+				createdDateTo,
+				pageNumber,
+				pageSize,
+				sortDirection,
+				sortAttribute
+				);
+		
+		return ResponseEntity
+				.ok(result);
 	}
 
 }
--- src/test/java/com/krzywe/Controllers/CalibrationSetControllerTest.java
+++ src/test/java/com/krzywe/Controllers/CalibrationSetControllerTest.java
@@ -2,10 +2,14 @@ package com.krzywe.Controllers;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -13,6 +17,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,6 +30,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -32,6 +41,7 @@ import com.krzywe.Config.ConfigAttributeMap;
 import com.krzywe.Config.ConfigManualProjection;
 import com.krzywe.DTO.CalibrationSetDTO;
 import com.krzywe.DTO.CalibrationSetView;
+import com.krzywe.DTO.SimpleCalibrationSetView;
 import com.krzywe.Model.CalibrationSet;
 import com.krzywe.Model.MaterialType;
 import com.krzywe.Service.ICalibrationSetService;
@@ -56,6 +66,7 @@ public class CalibrationSetControllerTest {
 	private ProjectionFactory factory;
 	
 	private CalibrationSetView calibrationSetView;
+	private SimpleCalibrationSetView simpleCalibrationSetView;
 	
 	private CalibrationSetDTO calibrationSetDTO;
 	
@@ -70,16 +81,17 @@ public class CalibrationSetControllerTest {
 		calibrationSetDTO.setExpirationDate(LocalDate.of(2023, 10, 1));
 		
 		var object = new CalibrationSet();
-		//object.setName(calibrationSetDTO.getName());
+		object.setName(calibrationSetDTO.getName());
 		object.setMaterialType(calibrationSetDTO.getMaterialType());
 		object.setPreparationDate(calibrationSetDTO.getPreparationDate());
 		object.setExpirationDate(calibrationSetDTO.getExpirationDate());
 		
 		calibrationSetView = factory.createProjection(CalibrationSetView.class, object);
+		simpleCalibrationSetView = factory.createProjection(SimpleCalibrationSetView.class, object);
 	}
 	
 	@Test
-	public void getObjectById() throws Exception {
+	public void create() throws Exception {
 		when(service.create(any(CalibrationSetDTO.class)))
 		.thenReturn(calibrationSetView);
 				
@@ -97,13 +109,91 @@ public class CalibrationSetControllerTest {
 				redirectedUrlPattern("**/v1/calibration_set/"+calibrationSetView.getId()),
 				jsonPath("$.name", is("name")),
 				jsonPath("$.materialType", is("CSF"))
-				);
-		
+				);	
+	}
+	
+	
+	@Test
+	public void update() throws Exception {
+		when(service.update(any(CalibrationSetDTO.class),anyString()))
+		.thenReturn(calibrationSetView);
+				
+		mvc.perform(
+				put("/v1/calibration_set/"+calibrationSetView.getId())
+				.with(csrf())
+				.with(user("admin").roles("ADMIN"))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(tester.write(calibrationSetDTO).getJson())
+				)
+		.andDo(print())
+		.andExpectAll(
+				status().isOk(),
+				content().contentType(MediaType.APPLICATION_JSON),
+				jsonPath("$.name", is("name")),
+				jsonPath("$.materialType", is("CSF"))
+				);		
 	}
 	
 	@Test
-	public void test() {
-		
+	public void findById() throws Exception {
+		when(service.findById(anyString()))
+		.thenReturn(calibrationSetView);
+				
+		mvc.perform(
+				get("/v1/calibration_set/"+calibrationSetView.getId())
+				.with(csrf())
+				.with(user("admin").roles("ADMIN"))
+				.contentType(MediaType.APPLICATION_JSON)
+				)
+		.andDo(print())
+		.andExpectAll(
+				status().isOk(),
+				content().contentType(MediaType.APPLICATION_JSON),
+				jsonPath("$.name", is("name")),
+				jsonPath("$.materialType", is("CSF"))
+				);	
 	}
+	
 
+	@Test
+	public void findAll() throws Exception {
+		var page = new PageImpl<SimpleCalibrationSetView>(
+				List.of(simpleCalibrationSetView),
+				PageRequest.of(0, 25),
+				1L);
+		when(service.findAll(
+				anyString(),
+				any(MaterialType.class),
+				any(LocalDate.class),
+				any(LocalDate.class),
+				any(LocalDateTime.class),
+				any(LocalDateTime.class),
+				anyInt(),
+				anyInt(),
+				any(Direction.class),
+				anyString()
+				)
+			)
+		.thenReturn(page);
+		System.out.println(page);
+		mvc.perform(
+				get("/v1/calibration_set")
+				.param("name", "name")
+				.param("materialType", "CSF")
+				.param("preparationDateFrom", "2020-01-01")
+				.param("preparationDateTo", "2021-01-01")
+				.param("createdDateFrom", "2020-01-01T08:30")
+				.param("createdDateTo", "2021-01-01T08:30")
+				.with(csrf())
+				.with(user("admin").roles("ADMIN"))
+				.contentType(MediaType.APPLICATION_JSON)
+				)
+		.andDo(print())
+		.andExpectAll(
+				status().isOk(),
+				content().contentType(MediaType.APPLICATION_JSON),
+				jsonPath("$.content[0].name", is("name")),
+				jsonPath("$.content[0].materialType", is("CSF"))
+				);	
+	}
 }
